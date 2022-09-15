0000d6c862044966f471c8b40cbd58da95cdb183
William Luck <luck.william.h@gmail.com>
2022-09-15 15:55:33 -0300

    commit tree 71bae8da5dc11335aa092e7d2950525251955da2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    # get all the games from the database 
+    games = Game.all.order(:title).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  # use the :id route here to create a dynamic route, using the params hash.
+  # If for exmaple make a get request to /games/10, the params hash would be { 'id' => '10' }
+  get '/games/:id' do 
+    # look up in the database using the ID, remember find method uses the ID 
+    # The params[:id] is just a way of getting 10 from the params hash.
+    game = Game.find(params[:id])
+    # includes all the reviews associated with the game in the JSON response
+    game.to_json(include: {reviews: { include: :user } })
   end
 
 end
